e5c6f2cbd7368795441d2d28d003c05e2338dcb5
DuongAnh1212 <tomnguyen6766@gmail.com>
2026-03-28 20:21:53 -0700

    add(api): update auth encode and decode token

--- gateway/app/config.py
+++ gateway/app/config.py
@@ -1,1 +1,18 @@
 #All settings from environment
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Settings():
+    #Gateway
+    secret_key: str= os.getenv("SECRET_KEY")
+    algorithm: str = os.getenv("ALGORITHM")
+    access_token_expire_minutes: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+    #CORS
+    cors_origin: str = os.getenv("CORS_ORIGIN")
+
+    #DATABASE
+    database_url: str = os.getenv("DATABASE_URL")
+
+settings= Settings()
